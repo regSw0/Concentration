f63e4632f78f5a69a64f194a21bc5e14448f7ad4
regSw0 <regina.sw06@gmail.com>
2023-04-28 20:10:13 +0400

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by User on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concentration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Concentration
+//
+//  Created by User on 28.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
